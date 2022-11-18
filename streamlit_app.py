8fdc72294e508ab1b5e7c3d8c35343094aaba271
Joy <118396868+packof8@users.noreply.github.com>
2022-11-18 09:18:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 
-# write your own comment -what does the next line do? 
+# Takes away the tic
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# Table
 streamlit.dataframe(fruityvice_normalized)
